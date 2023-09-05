d37210ca6204c0c2807073be7885a22ff7e5a2eb
sailaja ramaraju <sailajaramaraju@sailajas-MacBook-Air.local>
2023-09-05 23:49:11 +0530

    Initial copmmit

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,69 +1,47 @@
-/* eslint-disable*/
+/* eslint-disable */
 const todoList = () => {
-    var all = []
-    const add = (todoItem) => {
-      all.push(todoItem)
-    }
-    const markAsComplete = (index) => {
-      all[index].completed = true
-    }
-    const overdue = () => {
-      var overdue = []
-      for (let i = 0; i < all.length; i++) {
-        if (all[i].dueDate === yesterday) {
-            overdue.push(all[i]);
-        }
-      }
-      return overdue;
-    }
-    const dueToday = () => {
-        var dueToday = []
-        for (let i = 0; i < all.length; i++) {
-          if (all[i].dueDate === today) {
-              dueToday.push(all[i]);
-          }
-        }
-        return dueToday;
-    }
-    const dueLater = () => {
-        var dueLater = []
-        for (let i = 0; i < all.length; i++) {
-          if (all[i].dueDate === tomorrow) {
-              dueLater.push(all[i]);
-          }
-        }
-        return dueLater;
-    }
-    const toDisplayableList = (list) => {
-        let lists = '';
-        for (let i = 0; i < list.length; i++) {
-            if (list[i].completed === true) {
-                lists += '[x] ';
-                lists += list[i].title + " ";
-                if (list[i].dueDate !== today) {
-                lists += list[i].dueDate;
-                }
-                lists += "\n";
-            }
-            else if (list[i].completed !== true) {
-                lists += '[] ';
-                lists += list[i].title + " ";
-                if (list[i].dueDate !== today) {
-                lists += list[i].dueDate;
-                }
-                lists += "\n";
-            }
-        }
-        return lists;
-    }
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList
-    };
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
   };
-module.exports = todoList
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    const today = new Date().toLocaleDateString("en-CA");
+    return all.filter((item) => item.dueDate < today);
+  };
+  const dueToday = () => {
+    const today = new Date().toLocaleDateString("en-CA");
+    return all.filter((item) => item.dueDate === today);
+  };
+
+  const dueLater = () => {
+    const today = new Date().toLocaleDateString("en-CA");
+    return all.filter((item) => item.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+            todo.dueDate == today ? "" : todo.dueDate
+          }`,
+      )
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports = todoList;
